68c5f598ba2008a7108ed6c5a20da61ef9c5e286
Masahiro Fukuda <masahif@gmail.com>
2020-12-10 18:32:44 +0900

    changed: pushd -> cd

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,11 +6,11 @@ time=$(date)
 export PATH=/usr/bin:$PATH
 
 /usr/bin/yum install -y gcc zlib-devel bzip2 bzip2-devel readline-devel sqlite sqlite-devel \
-    openssl-devel tk-devel libffi-devel xz-devel git
+    openssl-devel tk-devel libffi-devel xz-devel git tar bzip2
 /usr/bin/git clone https://github.com/pyenv/pyenv.git ~/pyenv
 /usr/bin/mkdir /mnt/python/
 
-pushd ~/pyenv/plugins/python-build
+cd ~/pyenv/plugins/python-build
 ./install.sh
 /usr/local/bin/python-build 3.7.9 /mnt/python
 ARCHIVE_NAME='python-3.7.9-amzn2.tar.bz'
